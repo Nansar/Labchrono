c3f754ab6bb6d32b9d94f2290fd5506530419969
Nansar <haptel-sarah@outlook.com>
2023-07-25 15:30:58 +0200

    finish

---
+++ javascript/chronometer.js
@@ -0,0 +1,41 @@
+class Chronometer {
+    constructor() {
+        this.currentTime = 0;
+        this.internvalID = null;
+    }
+    start(callback) {
+        this.internvalID = setInterval(() => {
+            this.currentTime++;
+        }, 1000)
+    }
+    getMinutes() {
+        return Math.floor(this.currentTime / 60);
+    }
+    getSeconds() {
+        return Math.floor(this.currentTime % 60);
+    }
+    computeTwoDigitNumber(value) {
+        if (value < 10) {
+            return "0" + value;
+        }
+        else {
+            return '' + value;
+        }
+    }
+    stop() {
+        clearInterval(this.internvalID);
+    }
+    reset() {
+        this.currentTime = 0;
+    }
+    split() {
+        let minutos = this.computeTwoDigitNumber(this.getMinutes());
+        let segundos = this.computeTwoDigitNumber(this.getSeconds());
+        return `${minutos}${segundos}`;
+    }
+}
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
+if (typeof module !== 'undefined') {
+    module.exports = Chronometer;
+}
---
+++ javascript/index.js
@@ -0,0 +1,85 @@
+const chronometer = new Chronometer();
+
+const btnLeftElement = document.getElementById('btnLeft');
+const btnRightElement = document.getElementById('btnRight');
+
+const minDecElement = document.getElementById('minDec');
+const minUniElement = document.getElementById('minUni');
+const secDecElement = document.getElementById('secDec');
+const secUniElement = document.getElementById('secUni');
+const milDecElement = document.getElementById('milDec');
+const milUniElement = document.getElementById('milUni');
+const splitsElement = document.getElementById('splits');
+function printTime() {
+    setInterval(() => {
+        let segundos = printSeconds();
+        let minutos = printMinutes();
+        minDecElement.innerText = minutos[0];
+        minUniElement.innerText = minutos[1];
+        secUniElement.innerText = segundos[1];
+        secDecElement.innerText = segundos[0];
+    }, 1000);
+}
+function printMinutes() {
+    return chronometer.computeTwoDigitNumber(chronometer.getMinutes());
+}
+function printSeconds() {
+    return chronometer.computeTwoDigitNumber(chronometer.getSeconds());
+}
+
+function printMilliseconds() {
+
+}
+function printSplit() {
+    let marcaDeTiempo = chronometer.split();
+    console.log(marcaDeTiempo);
+    let elementoLista = document.createElement('li');
+    elementoLista.innerText = marcaDeTiempo;
+    splitsElement.appendChild(elementoLista);
+}
+function clearSplits() {
+    splitsElement.innerHTML = '';
+}
+function setStopBtn() {
+    btnLeftElement.innerText = 'STOP';
+    btnLeftElement.className = 'btn stop';
+}
+function setSplitBtn() {
+    btnRightElement.innerText = 'SPLIT';
+    btnRightElement.className = 'btn split';
+}
+function setStartBtn() {
+    btnLeftElement.innerText = 'START';
+    btnLeftElement.className = 'btn start';
+}
+function setResetBtn() {
+    btnRightElement.innerText = 'RESET';
+    btnRightElement.className = 'btn reset';
+}
+// Start/Stop Button
+btnLeftElement.addEventListener('click', () => {
+    if (btnLeftElement.innerText === 'START') {
+        setStopBtn();
+        chronometer.start();
+        printTime();
+        setSplitBtn();
+    } else {
+        setStartBtn();
+        setResetBtn();
+        chronometer.stop();
+    }
+});
+// Reset/Split Button
+btnRightElement.addEventListener('click', () => {
+    if (btnRightElement.innerText === 'RESET') {
+        chronometer.stop();
+        chronometer.reset();
+        minDecElement.innerText = '0';
+        minUniElement.innerText = '0';
+        secUniElement.innerText = '0';
+        secDecElement.innerText = '0';
+        clearSplits();
+    } else {
+        printSplit();
+    }
+})
